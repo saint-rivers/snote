53b2eca44e30c326abd3a942c96aadb74079c8f2
saint-rivers <eam.dayan@gmail.com>
2021-02-18 12:16:31 +0700

    refactored execute functions

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 stack_filename = dir_path + '/main_snote_entries.txt'
 
 # noinspection SpellCheckingInspection
-command_not_found_message = "command not listed" \
+command_not_found_message: str = "command not listed" \
                             "\nrun 'snote help' for commands I can run"
 
 
-def get_help_input():
+def get_help_input() -> object:
     try:
         command = str(input('Enter command: '))
         return command.replace(" ", "")
@@ -24,11 +24,49 @@ def get_help_input():
 
 
 def display_menu():
-    global user_command
     print("type 'stack' to enter entry mode")
     print("type 'read' to display all entries")
     print("type 'read simple' to display without timestamps")
-    user_command = get_help_input()
+
+
+def run_interface(filename):
+    display_menu()
+    run_command(get_help_input(), filename, "")
+
+
+def run_command(command, filename, entry):
+    try:
+        command_dict[command](filename, entry)
+        return
+    except TypeError:
+        command_dict[command](filename)
+        return
+    except KeyError:
+        print(command_not_found_message)
+        return
+
+    command_dict[command]()
+
+
+def read_user_commands():
+    command = ""
+    try:
+        for i in range(1, len(sys.argv)):
+            command += str(sys.argv[i])
+    except TypeError:
+        print(command_not_found_message)
+    return command
+
+
+def get_quick_entry():
+    entry = ""
+    try:
+        for i in range(2, len(sys.argv)):
+            entry += str(sys.argv[i]) + " "
+    except IndexError:
+        print(command_not_found_message)
+    finally:
+        return entry
 
 
 command_dict = dict(
@@ -36,36 +74,25 @@ command_dict = dict(
     stack=snack.stack_run,
     reads=snack.read_simple,
     readsimple=snack.read_simple,
-    help=display_menu,
-    q=snack.quick_write
+    help=run_interface
 )
 
 if __name__ == '__main__':
 
-    # combine user's space separated input into command
     user_command = ""
-    entry = ""
+    q_entry = ""
 
+    # run default function
     if len(sys.argv) == 1:
-        snack.stack_run(stack_filename) # run default function
-        exit(0)
-    elif sys.argv[1] == 'help':
-        display_menu()
-    elif sys.argv[1] != '':
-        for i in range(1, len(sys.argv)):
-            user_command += str(sys.argv[i])
-    else:
-        try:
-            user_command = str(sys.argv[1])
-            for i in range(2, len(sys.argv)):
-                entry += str(sys.argv[i]) + " "
-        except IndexError:
-            print(command_not_found_message)
+        user_command = 'stack'
 
-    # run that command
-    try:
-        command_dict[user_command](stack_filename, entry)
-    except TypeError:
-        command_dict[user_command](stack_filename)
-    except KeyError:
-        print(command_not_found_message)
+    # check if it is a quick entry
+    elif sys.argv[1] == 'q':
+        # add every subsequent parameters as an entry
+        q_entry = get_quick_entry()
+
+    # if it's not a quick entry
+    else:
+        user_command = read_user_commands()
+
+    run_command(user_command, stack_filename, q_entry)
